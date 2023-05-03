002d98dbe90ced76dd5e6a46ff4fbf6a75e0d85c
Łukasz Borowski <242124@edu.p.lodz.pl>
2023-05-03 02:55:57 +0200

    initial commit

---
+++ src/app/tre/tre.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tre',
+  templateUrl: './tre.component.html',
+  styleUrls: ['./tre.component.css']
+})
+export class TreComponent {
+
+}
