3d210a43cd0ea72b9f3914617b847023a036e54e
Łukasz Borowski <242124@edu.p.lodz.pl>
2023-05-04 13:59:12 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { QuadraticComponent } from './quadratic/quadratic.component';
-import { TreComponent } from './tre/tre.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuadraticComponent,
-    TreComponent
   ],
   imports: [
     BrowserModule
